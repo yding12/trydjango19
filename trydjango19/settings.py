6839db1596ef407889184e647a2929ab8424caa6
yding12 <yding9310@gmail.com>
2016-07-30 04:46:40 +0800

    Add files via upload
    static file

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'posts',
 ]
 
@@ -121,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+   # '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+#os.path.dirname(BASE_DIR) the directory name one up from the trydjango19 project
