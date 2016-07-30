33bc5f451c60d5c71358089b0e5ab74e1ac4ad59
yding12 <yding9310@gmail.com>
2016-07-31 06:44:45 +0800

    Add files via upload
    upload images, put image files in media directory. create set up for media

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -129,5 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 #os.path.dirname(BASE_DIR) the directory name one up from the trydjango19 project
