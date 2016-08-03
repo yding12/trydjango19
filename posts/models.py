d328cd87c1bdfb480699484b9a701b5f4d9cf058
yding12 <yding9310@gmail.com>
2016-08-03 15:39:19 -0700

    Add files via upload
    Add Slug Field and post_create works

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
@@ -11,6 +11,7 @@ def upload_loc(instance, filename):
 
 # Create your models here.
 class Post(models.Model):
+
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_loc,
@@ -42,12 +43,14 @@ def create_slug(instance, new_slug=None):
     exists = qs.exists()
     if exists:
         new_slug = "%s-%s" %(slug, qs.first().id)
-        return create_slug(instance,new_slug=new_slug)
+        return new_slug
     return slug
 
 
-def pre_save_post_reveiver(sender, instance,*atgs,**kwargs):
+
+def pre_save_post_receiver(sender,instance,*args,**kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
 
-pre_save.connect(pre_save_post_reveiver,sender=Post)
+
+pre_save.connect(pre_save_post_receiver,Post)
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from django.contrib import admin
-from . import views
+
+
 from .views import(
     post_detail,
     post_create,
@@ -12,19 +12,17 @@ from .views import(
 urlpatterns = [
 
     url(r'^$', post_list, name='list'),
-    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^create/$', post_create),
+    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
 
+    # url(r'^$', post_list, name='list'),
+    # url(r'^create/$', post_create),
+    # url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+    # url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
+    # url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
+
+
 ]
 
-# urlpatterns = [
-#
-#     url(r'^$', "posts.views.post_home"),
-#     url(r'^create/$', "posts.views.post_create"),
-#     url(r'^delete/$', "posts.views.post_delete"),
-#     url(r'^update/$', "posts.views.post_update"),
-#     url(r'^list/$', "posts.views.post_list"),
-#     #url(r'^posts/$', "<appname>.view.<function_name>")
-# ]
